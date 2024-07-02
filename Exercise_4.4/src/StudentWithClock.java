d81806196ef0b29a975d415490db48d30bbbbfa2
FrtyNock <vladimir.nurov43@mail.ru>
2024-07-02 14:19:35 +0300

    Add files via upload

---
+++ Exercise_4.4/src/StudentWithClock.java
@@ -0,0 +1,18 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class StudentWithClock implements Learner {
+    private Learner learner;
+
+    public StudentWithClock(Learner learner) {
+        this.learner = learner;
+    }
+
+    @Override
+    public void learn() {
+        SimpleDateFormat ft = new SimpleDateFormat("HH.mm.ss");
+        learner.learn();
+        Date date = new Date();
+        System.out.println("Текущее время: " + ft.format(date));
+    }
+}
